7fa6b3fd996faf094624e4920834ed3f9fad0ede
lovecoding-git <fedde198212@gmail.com>
2023-03-18 16:59:59 -0400

    commit

--- src/views/dashboard/AllProjects/ProjectItem.js
+++ src/views/dashboard/AllProjects/ProjectItem.js
@@ -1,65 +1,65 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import DeleteIcon from '@material-ui/icons/Delete';
-import {DeleteConfirmDialog} from './DeleteConfirmDialog';
+import { DeleteConfirmDialog } from './DeleteConfirmDialog';
 import ConfirmPassword from './ConfirmPassword'
-import {withRouter} from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const styles = {
-    root:{
-        width:'100%',
+    root: {
+        width: '100%',
     },
-    row:{
-        width:'calc(100% - 20px)',
-        backgroundColor:'#3030307f',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        height:'50px',
-        margin:'auto',
-        marginTop:'10px',
-        marginBotton:'10px',
-        fontSize:'20px',
-        fontWeight:'bold',
-        textTransform:'capitalize',
-        borderRadius:'4px',
-        boxShadow:'0 0 2px 1px #00000033',
-        position:'relative',
+    row: {
+        width: 'calc(100% - 20px)',
+        backgroundColor: '#3030307f',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50px',
+        margin: 'auto',
+        marginTop: '10px',
+        marginBotton: '10px',
+        fontSize: '20px',
+        fontWeight: 'bold',
+        textTransform: 'capitalize',
+        borderRadius: '4px',
+        boxShadow: '0 0 2px 1px #00000033',
+        position: 'relative',
     },
-    link:{
-        textDecoration:'none',
-        color:'white'
+    link: {
+        textDecoration: 'none',
+        color: 'white'
     },
-    deleteButtonContainer:{
-        position:'absolute',
+    deleteButtonContainer: {
+        position: 'absolute',
         right: 0,
         width: 50,
         height: '100%',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    deleteIcon:{
-        borderRadius:'9999px',
-        backgroundColor:'rgb(0, 151, 62)',
+    deleteIcon: {
+        borderRadius: '9999px',
+        backgroundColor: 'rgb(0, 151, 62)',
         padding: 5,
         fontSize: 40,
-        color:'#ff0000'
+        color: '#ff0000'
     },
-    deleteIconInActive:{
-        borderRadius:'9999px',
-        backgroundColor:'rgb(0, 151, 62, 0.5)',
+    deleteIconInActive: {
+        borderRadius: '9999px',
+        backgroundColor: 'rgb(0, 151, 62, 0.5)',
         padding: 5,
         fontSize: 40,
-        color:'#ff00007f'
+        color: '#ff00007f'
     }
 }
 
 const ProjectItem = (props) => {
 
-    const {project, history} = props;
-    const user = useSelector(state=>state.authUser.user)
+    const { project, history } = props;
+    const user = useSelector(state => state.authUser.user)
     const [open, setOpen] = React.useState(false)
     const [openpassword, setOpenPassword] = React.useState(false)
 
@@ -68,43 +68,43 @@ const ProjectItem = (props) => {
     }
 
     const openProjectDetail = () => {
-        if(project.userId === user.id){
+        if (project.userId === user.id) {
             history.push(`/report-project/${project.id}`)
-        }else{
+        } else {
             setOpenPassword(true)
         }
     }
 
-    const handleClosePassword = () =>{
+    const handleClosePassword = () => {
         setOpenPassword(false)
     }
 
     return (
         <div style={styles.root}>
             <div style={styles.row}>
-                <div onClick={openProjectDetail} style={{color:'white'}}>
+                <div onClick={openProjectDetail} style={{ color: 'white' }}>
                     {project.projectName}
                 </div>
                 {
-                    (user.id == project .userId) ?
-                    <div style={styles.deleteButtonContainer} onClick= {()=>{setOpen(true)}} >
-                        <DeleteIcon style={styles.deleteIcon} />
-                    </div>:
-                    <div style={styles.deleteButtonContainer}>
-                        <DeleteIcon style={styles.deleteIconInActive} />
-                    </div>
+                    (user.id == project.userId) ?
+                        <div style={styles.deleteButtonContainer} onClick={() => { setOpen(true) }} >
+                            <DeleteIcon style={styles.deleteIcon} />
+                        </div> :
+                        <div style={styles.deleteButtonContainer}>
+                            <DeleteIcon style={styles.deleteIconInActive} />
+                        </div>
                 }
-                
+
             </div>
             <DeleteConfirmDialog
-                open = {open}
-                handleClose = {handleClose}
-                project = {project}
+                open={open}
+                handleClose={handleClose}
+                project={project}
             />
             <ConfirmPassword
-                open = {openpassword}
-                handleClose = {handleClosePassword}
-                project = {project}
+                open={openpassword}
+                handleClose={handleClosePassword}
+                project={project}
             />
         </div>
     )
--- src/views/dashboard/AllProjects/ProjectList.js
+++ src/views/dashboard/AllProjects/ProjectList.js
@@ -5,41 +5,41 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import ProjectItem from './ProjectItem'
 import AddIcon from '@material-ui/icons/Add';
 import { Link } from 'react-router-dom'
-import {makeStyles} from '@material-ui/styles'
+import { makeStyles } from '@material-ui/styles'
 
-const useStyles = makeStyles(()=>({
-    root:{
-        width:'100%',
+const useStyles = makeStyles(() => ({
+    root: {
+        width: '100%',
         marginTop: '60x',
     },
-    progressContaner:{
+    progressContaner: {
         display: 'flex',
-        backgroundColor:'white',
-        justifyContent:'center',
-        alignItems:'center',
-        height:'100vh',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
     },
-    
-    addNewProject:{
-        width:'50px',
-        backgroundColor:'#00973e',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        height:'50px',
-        margin:'auto',
-        marginTop:'10px',
-        marginBotton:'10px',
-        fontSize:'50px',
-        fontWeight:'bold',
-        color:'white',
-        textTransform:'capitalize',
-        borderRadius:'50%',
-        boxShadow:'0 0 2px 1px #0000007f',
-        position:'fixed',
+
+    addNewProject: {
+        width: '50px',
+        backgroundColor: '#00973e',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50px',
+        margin: 'auto',
+        marginTop: '10px',
+        marginBotton: '10px',
+        fontSize: '50px',
+        fontWeight: 'bold',
+        color: 'white',
+        textTransform: 'capitalize',
+        borderRadius: '50%',
+        boxShadow: '0 0 2px 1px #0000007f',
+        position: 'fixed',
         bottom: 20,
         right: 'calc(50% - 482px)',
-        '@media screen and (max-width: 1024px)':{
+        '@media screen and (max-width: 1024px)': {
             right: 20,
         }
     }
@@ -47,34 +47,33 @@ const useStyles = makeStyles(()=>({
 
 const ProjectList = () => {
     const classes = useStyles();
-    const user = useSelector(state=>state.authUser.user);
-    useFirestoreConnect([{collection: 'projects'}])
-    const projects = useSelector(state => state.firestore.ordered.projects) 
-    console.log(projects)
+    useFirestoreConnect([{ collection: 'projects' }])
+    const projects = useSelector(state => state.firestore.ordered.projects)
+
     return (
         <div className={classes.root}>
             {
-                projects?
-                <div className={{paddingTop: 10}}>
-                    {
-                        projects.map((project)=>(
-                            <div key={project.id}>
-                                <ProjectItem project = {project} />
+                projects ?
+                    <div className={{ paddingTop: 10 }}>
+                        {
+                            projects.map((project) => (
+                                <div key={project.id}>
+                                    <ProjectItem project={project} />
+                                </div>
+                            ))
+                        }
+                        <Link to="/add-project">
+                            <div className={classes.addNewProject}>
+                                <AddIcon style={{ fontSize: '50px' }} />
                             </div>
-                        ))
-                    }
-                    <Link to="/add-project">
-                        <div className={classes.addNewProject}>
-                            <AddIcon style={{fontSize:'50px'}} />
-                        </div>
-                    </Link>
-                </div>:
-                <div className={classes.progressContaner}>
-                    <CircularProgress />
-                </div>
+                        </Link>
+                    </div> :
+                    <div className={classes.progressContaner}>
+                        <CircularProgress />
+                    </div>
             }
         </div>
     )
 }
 
-export default  ProjectList;
+export default ProjectList;
